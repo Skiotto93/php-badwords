968195626178eb7cd8f996d071960722d380a806
skiotto93 <fabio.forenza@hotmail.com>
2022-12-13 18:31:36 +0100

    Aggiunto testo e titolo

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+    $song_text = "My cock is much bigger than yours
+                    My cock can walk right through the door
+                    With a feeling so pure
+                    It's got you screaming back for";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Badwords</title>
+    </head>
+    <body>
+        <h1>Cigaro - System of a down</h1>
+        <p> <?php echo $song_text ?> </p>
+        <div>
+            <h4>
+                Lunghezza testo: <?php echo strlen($song_text) ?>
+            </h4>
+        </div>
+    </body>
+</html>
